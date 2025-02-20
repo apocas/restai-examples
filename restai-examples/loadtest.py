a3e977a0285722c9c13f82a1760ab571b943ef37
Pedro Dias <petermdias@gmail.com>
2025-02-20 23:46:14 +0000

    initial commit

---
+++ restai-examples/loadtest.py
@@ -0,0 +1,9 @@
+from restai_functions import Restai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+restai = Restai(url=os.environ.get("RESTAI_URL"), api_key=os.environ.get("RESTAI_KEY"))
+
+print(restai.pedro_inference("Hi I'm Pedro"))
